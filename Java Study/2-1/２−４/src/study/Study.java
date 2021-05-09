53ea549661cde1e8cc87c418c219f62a09572df9
jamie-1123 <78092505+jamie-1123@users.noreply.github.com>
2021-05-09 19:44:02 +0900

    課題提出

---
+++ Java Study/2-1/２−４/src/study/Study.java
@@ -0,0 +1,32 @@
+package study;
+
+/**
+ * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+ *
+ * 問①〜問③まであります。
+ * 全て回答し、課題にある画像と同じ表示になるようにしてください。
+ *
+ */
+public class Study {
+
+
+
+
+
+	public static void main(String[] args) {
+
+        /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+         *     また、課題の画像と同じ表示になるように
+         *     printFruitsの引数にはFruitsConstantsに指定されてる定数を設定してください。
+         */
+
+		final String FRUITS_APPLE_01 = "りんご";
+		final int FRUITS_COUNT_20 = 20;
+
+
+		System.out.println("食べ物は" + FRUITS_APPLE_01);
+		System.out.println(FRUITS_COUNT_20 + "個です");
+
+
+    }
+}
