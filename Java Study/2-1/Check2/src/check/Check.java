6e43bde78d7d1a890e573fee144bdc5747978d27
jamie-1123 <78092505+jamie-1123@users.noreply.github.com>
2021-06-08 20:43:14 +0900

    課題提出

--- Java Study/2-1/Check2/src/check/Check.java
+++ Java Study/2-1/Check2/src/check/Check.java
@@ -4,33 +4,32 @@ import constants.Constants;
 //課題①【Check.java】にてフィールド変数firstNameとlastNameを宣言し、firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
 //なお、変数のアクセス修飾子は「private」とする。
 
-//課題②【Check.java】にてfirstNameとlastNameを引数で受け取って、連結して表示させるメソッド「printName」を作成しなさい。
-//作成した関数のアクセス修飾子は「private」とする。
+
 
 
 public class Check {
-	 private  String firstName = "sugiyama";
-	 private  String lastName = "jamie";
+	 private  static String firstName = "sugiyama";
+	 private  static String lastName = "jamie";
 
 
 
 
 	//String Check= firstName + lastName;
 
+	//課題②【Check.java】にてfirstNameとlastNameを引数で受け取って、連結して表示させるメソッド「printName」を作成しなさい。
+	//作成した関数のアクセス修飾子は「private」とする。
 
-private void printName() {
-	System.out.println("printメソッド" + "→" + printName(firstName,lastName));
+private static void printName(String firstName,String lastName) {
+	System.out.println("printメソッド" + "→" + firstName + lastName);
 }
 
-	 private static String printName(String firstName ,String lastName) {
-		 return firstName + lastName;
 
-	 }
 
+
+//【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
 
 	public static void main(String[] args) {
-		Check c = new Check();
-		c.printName();
+		Check.printName(firstName,lastName);
 
 
 		Pet p = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
